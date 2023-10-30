bd7df3e951eb7a73ea5c4a4f0f30aadde86e61f3
sav <sav@r-p-s.ru>
2023-10-30 08:56:00 +0200

    chapter 1.2

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
+
+	var s, sep string
+
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+
+	for j, arg := range os.Args[0:] {
+		fmt.Printf("%d %s\n", j, arg)
+	}
+
 	fmt.Println("Hello, МИР")
+	fmt.Println(s)
+	fmt.Println(os.Args[0:])
+	fmt.Println(strings.Join(os.Args[0:], " "))
+
 }
